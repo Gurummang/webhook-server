387754c63fac681041ff17afb964f57ec824c8d3
2W <hsp003636@gmail.com>
2024-07-13 15:57:19 +0900

    init

---
+++ src/main/java/com/grummang/webhook_server/controller/WebhookController.java
@@ -0,0 +1,4 @@
+package com.grummang.webhook_server.controller;
+
+public class WebhookController {
+}
---
+++ src/main/java/com/grummang/webhook_server/service/SlackEventService.java
@@ -0,0 +1,4 @@
+package com.grummang.webhook_server.service;
+
+public class SlackEventService {
+}
