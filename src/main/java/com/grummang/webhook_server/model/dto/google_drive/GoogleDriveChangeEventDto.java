889085b02b5b960deded9b5c0afc45351cb664f0
2W <hsp003636@gmail.com>
2024-09-17 23:23:42 +0900

    KAN-479 <add : 구글 드라이브 이벤트 dto 생성 >

--- src/main/java/com/grummang/webhook_server/model/dto/google_drive/GoogleDriveChangeEventDto.java
+++ src/main/java/com/grummang/webhook_server/model/dto/google_drive/GoogleDriveChangeEventDto.java
@@ -1,4 +1,12 @@
 package com.grummang.webhook_server.model.dto.google_drive;
 
+import lombok.Data;
+
+@Data
 public class GoogleDriveChangeEventDto {
+    private String workspaceId;
+    private String channelId;
+    private String resourceId;
+    private String resourceState;
+    private String resourceUri;
 }
